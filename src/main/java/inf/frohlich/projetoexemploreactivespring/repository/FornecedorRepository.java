70e04ef3e25bbfea08608a171b80721a868101a4
ricardofrohlich <ricardosma@gmail.com>
2024-09-12 20:02:22 -0300

    Corrigindo o repository que fizeram ontem

--- src/main/java/inf/frohlich/projetoexemploreactivespring/repository/FornecedorRepository.java
+++ src/main/java/inf/frohlich/projetoexemploreactivespring/repository/FornecedorRepository.java
@@ -3,5 +3,5 @@ package inf.frohlich.projetoexemploreactivespring.repository;
 import inf.frohlich.projetoexemploreactivespring.model.Fornecedor;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 
-public interface FornecedorRepository extends R2dbcRepository<Fornecedor,Long>{{
+public interface FornecedorRepository extends R2dbcRepository<Fornecedor,Long>{
 }
