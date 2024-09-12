c8e2048d52b91259dff8dc859e598201449f7532
Ricardo Frohlich da Silva <ricardosma@gmail.com>
2024-09-12 19:57:50 -0300

    Finalizando o desenvolvimento do Repository que o Ricardo iniciou

--- src/main/java/inf/frohlich/projetoexemploreactivespring/repository/FornecedorRepository.java
+++ src/main/java/inf/frohlich/projetoexemploreactivespring/repository/FornecedorRepository.java
@@ -1,4 +1,7 @@
 package inf.frohlich.projetoexemploreactivespring.repository;
 
-public class FornecedorRepository {
+import inf.frohlich.projetoexemploreactivespring.model.Fornecedor;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+
+public interface FornecedorRepository extends R2dbcRepository<Fornecedor,Long>{{
 }
